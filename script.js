d27446d8f8cffef969cbc27913eb6ac3dfce4705
Rajakumar Kalakaraddi <reddyrajkumar2025@gmail.com>
2025-12-01 10:47:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 fetch("https://api.github.com/users/raj94-cloud/repos?sort=updated")
-  .then(response => response.json())
-  .then(data => {
-    let repoContainer = document.getElementById("repo-list");
-    repoContainer.innerHTML = "";
+    .then(res => res.json())
+    .then(data => {
+        let container = document.getElementById("repo-list");
+        container.innerHTML = "";
 
-    data.forEach(repo => {
-      let card = `
-        <div class="repo-card">
-            <h3>${repo.name}</h3>
-            <p>${repo.description || "No description provided"}</p>
-            <a href="${repo.html_url}" target="_blank">🔗 Open Repository</a>
-        </div>
-      `;
-      repoContainer.innerHTML += card;
+        data.forEach(repo => {
+            container.innerHTML += `
+                <div class="project-card">
+                    <h3>${repo.name}</h3>
+                    <p>${repo.description || "No description provided"}</p>
+                    <a href="${repo.html_url}" target="_blank">View Repository</a>
+                </div>
+            `;
+        });
     });
-  })
-  .catch(err => {
-    document.getElementById("repo-list").innerHTML = "Error loading repositories.";
-    console.error(err);
-  });
